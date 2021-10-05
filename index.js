615e39cd33078b95899fa867f9a2b737d01222f9
Master Li <61086690+X-DDDDD@users.noreply.github.com>
2021-10-05 10:09:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const readme = `\
 
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
----
+---  
 
 ⏰ Updated on ${new Date().toUTCString()}
----
+---  
 
 console.log(readme)
