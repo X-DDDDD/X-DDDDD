4d50b933eac5a50248bfd23431c2a4c405eca8ef
Master Li <61086690+X-DDDDD@users.noreply.github.com>
2021-10-05 10:34:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function generateProgressBar() {
 }
 
 const readme = `\
-### Hi there 👋
+## Hi there 👋, I'm am ordinary engineering students.
 
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
@@ -22,6 +22,24 @@ const readme = `\
 
 ⏰ Updated on ${new Date().toUTCString()}
 
+---
+
+### My GitHub Contributions  
+
+![](https://raw.githubusercontent.com/X-DDDDD/X-DDDDD/main/assets/github-contribution-grid-snake.svg)  
+
+### About me      
+
+[![GitHub stats](https://github-readme-stats.vercel.app/api?username=X-DDDDD&show_icons=true&theme=radical)](https://github.com/anuraghazra/github-readme-stats)
+![Most used languages](https://github-readme-stats.vercel.app/api/top-langs/?username=X-DDDDD&layout=compact&hide_border=true&langs_count=10)  
+
+:) --- 2021-10-05
+    
+- 🤔 My blog 👉 https://blog.csdn.net/whatever_ittakes        
+- 📫 My E-mail: 1336143736#qq.com          
+- 🔧 My Power Tools: </br>   
+![](https://img.shields.io/badge/%E5%86%99%E4%BD%9C%E5%B7%A5%E5%85%B7-VS%20Code-blue)     
+ 
 `
 
 console.log(readme)
